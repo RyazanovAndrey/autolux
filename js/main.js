c09223b112359bb46d5dcbf902edd5995915aeeb
RyazanovAndrey <ryazanov.andrej@gmail.com>
2023-02-20 22:09:08 +0200

    22.10

--- js/main.js
+++ js/main.js
@@ -9,3 +9,20 @@ btn.addEventListener("click", function(event){
     btn.classList.toggle("btn__active");
 })
 
+// tabs
+
+const tab = document.querySelectorAll(".tabs__item");
+const tabContent = document.querySelectorAll(".tabs__content");
+
+tab.forEach(item => {
+    item.addEventListener("click", ()=> {
+
+        tab.forEach(el => el.classList.remove("active__tab"));
+        tabContent.forEach(cl => cl.classList.add("hidden"));
+        
+        item.classList.add("active__tab");
+
+        const opnTab = document.querySelector("#" + item.dataset.tab);
+        opnTab.classList.remove("hidden");
+    })
+})
